65dd794c32698feee6b6d7d8ad841f8647683346
GtrCraft <nickvanbokhorst@hotmail.com>
2017-05-17 16:28:32 +0200

    Rebase to omni

--- omni_potter.mk
+++ omni_potter.mk
@@ -15,8 +15,8 @@
 #
 
 # Inherit from our custom product configuration
-#$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 $(call inherit-product, device/motorola/potter/full_potter.mk)
 
-PRODUCT_NAME := lineage_potter
+PRODUCT_NAME := omni_potter
